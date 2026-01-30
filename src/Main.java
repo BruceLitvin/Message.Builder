2daf807d96d892666339c6218e4a0c2dff0805dc
BruceLitvin <brucel15@nycstudents.net>
2026-01-30 12:31:14 -0500

    Stuff

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-MessageBuilder a = new MessageBuilder("Ilovehotdogs");
+MessageBuilder a = new MessageBuilder("Ilo v e h o tdogs");
         System.out.println(a.num());
     }
 
--- src/MessageBuilder.java
+++ src/MessageBuilder.java
@@ -5,9 +5,16 @@ private int numWords;
 public MessageBuilder(String startingWord)
 {
     message= startingWord;
+    for (int i = 0; i < message.length() ; i++) {
+        if(message.substring(i,i+1).equals("")){
+            numWords++;
+        }
+    }
 
 }
-
+public int num(){
+    return numWords;
+}
 //public String getNextWord(String s)
 //{ return null;}
 
