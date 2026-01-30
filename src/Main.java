fa7c8a3a4bb7fcac428a84b4cbd5e070d9d5f2d8
Bruce Litvin <brucel15@nycstudents.net>
2026-01-30 12:26:03 -0500

    Skeleton

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+MessageBuilder a = new MessageBuilder("Ilovehotdogs");
+        System.out.println(a.num());
+    }
+
+
+}
---
+++ src/MessageBuilder.java
@@ -0,0 +1,20 @@
+public class MessageBuilder{
+private String message;
+private int numWords;
+
+public MessageBuilder(String startingWord)
+{
+    message= startingWord;
+
+}
+public int num()
+    {
+        return numWords;
+    }
+//public String getNextWord(String s)
+//{ return null;}
+
+//public String getAbbreviation()
+//{  }
+
+}
