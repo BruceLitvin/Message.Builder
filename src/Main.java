e72c59f0bbfc31b1181c19ea1bcf3c3b9bc80cdd
BruceLitvin <brucel15@nycstudents.net>
2026-02-01 09:42:25 -0500

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[] args) {
-MessageBuilder a = new MessageBuilder("Ilo v e h o tdogs");
-        System.out.println(a.num());
+MessageBuilder a = new MessageBuilder("Never Gonna Give You Up");
+        System.out.println(a.getAbbreviation());
     }
 
 
--- src/MessageBuilder.java
+++ src/MessageBuilder.java
@@ -4,21 +4,39 @@ private int numWords;
 
 public MessageBuilder(String startingWord)
 {
+    numWords=1;
     message= startingWord;
-    for (int i = 0; i < message.length() ; i++) {
-        if(message.substring(i,i+1).equals("")){
-            numWords++;
-        }
+    String next= getNextWord(startingWord);
+    while (next!= null){
+        message += " "+next;
+        numWords++;
+        next=getNextWord(next);
     }
 
 }
-public int num(){
-    return numWords;
-}
-//public String getNextWord(String s)
-//{ return null;}
 
-//public String getAbbreviation()
-//{  }
+public String getNextWord(String s)
+{
+    return null;
+}
+
+public String getAbbreviation()
+{
+    String abbr = "";
+    int index=0;
+    while (index < message.length()){
+        abbr+= message.substring(index,index+1);
+        int space= message.indexOf(" ", index);
+        if(space==-1)
+        {
+            index=message.length();
+        }
+        else {
+            index=space+1;
+        }
+    }
+
+    return abbr;
+}
 
 }
